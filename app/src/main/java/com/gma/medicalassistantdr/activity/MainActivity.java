75aa3b297650c578644750c0b61b2710dbd019e6
Lionel Shen <lionel.shen@autodesk.com>
2018-09-12 22:00:19 +0800

    Added login

---
+++ app/src/main/java/com/gma/medicalassistantdr/activity/MainActivity.java
@@ -0,0 +1,49 @@
+package com.gma.medicalassistantdr.activity;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.gma.medicalassistantdr.R;
+import com.gma.medicalassistantdr.utils.MedConst;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean mLogged = false;
+    private String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //mLogged = true;
+        if (!mLogged) {
+            Intent loginIntent = new Intent(MedConst.INTENT_ACTION_LOGIN);
+            loginIntent.putExtra("key", "test");
+            int requestCode = MedConst.LOGIN_REQUEST_CODE;
+            startActivityForResult(loginIntent, requestCode);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case MedConst.LOGIN_REQUEST_CODE:
+                if (resultCode == RESULT_OK) {
+                    String result = data.getExtras().getString("result");
+                    mLogged = result.equals("OK");
+                    Log.i(TAG, result);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/gma/medicalassistantdr/utils/MedConst.java
@@ -0,0 +1,10 @@
+package com.gma.medicalassistantdr.utils;
+
+public class MedConst {
+    // Intent
+    public static final String INTENT_ACTION_LOGIN = "ACTION_LOGIN";
+
+    // Request Code
+    public static final int LOGIN_REQUEST_CODE = 10001;
+
+}
